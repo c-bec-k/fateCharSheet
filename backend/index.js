ad92bc298b31bc1508a56d1926f2f7dfdb53b856
c-bec-k <cruthenbeck@gmail.com>
2022-08-28 14:02:11 -0400

    static content

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,64 @@
-import http from 'node:http';
+import {createServer} from 'node:http';
+import {env} from 'node:process';
+import {extname, parse} from 'node:path';
+import {readFile} from 'node:fs/promises';
 
-import { pipeHTTP } from './lib/server.js';
-import { tee } from './lib/utils.js';
+const contentTypes = {
+  '.css': 'text/css',
+  '.js' : 'text/javascript',
+  '.html': 'text/html'
+}
 
-const srv = http.createServer({captureRejections: true});
-srv.listen(9001, () => console.log("Listening on port 9001"));
+const server = createServer().listen( env.PORT || 9001, () => console.log("Server running on port", env.PORT || 9001));
 
-const logReq = (obj) => console.log(obj.req.headers);
-const rejectMe = ({req, res, ctx}) => Promise.reject({req, res, ctx: {...ctx, errorCode: 500, errMsg: "Internal my bad. Oops!"}});
-const sendOK = ({req, res, ctx}) => res.end("Thank you for visiting today!");
-const catchHandler = ({req,res,ctx}) => {
-  res.statusCode = ctx.errorCode;
-  res.end(ctx.errMsg);
+const parseReqBody = async ({req, res, ctx}) =>  {
+  if (!req['headers']['content-type']) return {req, res, ctx};
+  let data = '';
+  for await (const chunk of req) {
+    data += chunk;
+  }
+  ctx.set('reqBody', data);
+  return {req, res, ctx};
 };
 
-const promHTTP = (req, res) => Promise.resolve({req, res, ctx: {}})
-                               .then(tee(logReq))
-                               .then(rejectMe)
-                               .then(sendOK)
-                               .catch(catchHandler)
 
-srv.on('request', promHTTP);
+const serveStatic = async ({req, res, ctx}) => {
+  const reqUrl = req.url;
+  if (reqUrl === '/favicon.ico') return {req,res,ctx};
+  console.log(reqUrl);
+  let dir = '../frontend';
+  if (reqUrl === '/') {
+    dir += '/index.html';
+    ctx.set('Content-Type', 'text/html');
+    ctx.set('statusCode', 200);
+    const reqFile = await readFile(dir);
+    ctx.set('resBody', reqFile);
+  } else {
+    dir += reqUrl;
+    try {
+      const reqFile = await readFile(dir);
+      ctx.set('Content-Type', contentTypes[extname(dir)]);
+      ctx.set('resBody', reqFile);
+      ctx.set('statusCode', 200); 
+    } catch {
+      console.error(`Requested ${dir} does not exist.`);
+    }
+  }
+
+  return {req,res,ctx};
+}
+
+const sendResponse = ({res,ctx}) => {
+  res.setHeader('Content-Type', ctx.get('Content-Type') || 'application/octet-stream');
+  res.statusCode = ctx.get('statusCode') || 500;
+  res.end(ctx.get('resBody'));
+};
+
+
+const HTTPhandler = (req, res) => Promise.resolve({req, res, ctx: new Map()})
+  .then(parseReqBody)
+  .then(serveStatic)
+  .then(sendResponse)
+
+
+server.on('request', HTTPhandler);
--- backend/lib/server.js
+++ backend/lib/server.js
@@ -1,5 +1,1 @@
-// const pipeAsync = (...fns) => (...init) => fns.reduce( (val, fn) => fn(val), Promise.resolve(...init));
 
-const pipeHTTP = (...fns) => (req, res) => fns.reduce( (promise, fn) => promise.then(fn), Promise.resolve({req,res}));
-
-export { pipeHTTP }
